081f22d0b468514640cded001cb85dce444304e1
Juan Suñe <111535673+JuanSune@users.noreply.github.com>
2023-09-05 19:39:38 -0300

    d

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-import NovaIdade from './components/NovaIdade';
+import logo from "./logo.svg";
+import "./App.css";
+import NovaIdade from "./components/NovaIdade";
+import { useState, useEffect } from "react";
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
+const API = "https://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [time, setTime] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
-      <NovaIdade></NovaIdade>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      <div className="todo-form">
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <label></label>
 
+          </div>
+          <input type="submit" value='Enviar'/>
+
+        </form>
+      </div>
+      <div className="todo-list">
+        <h2>tividade</h2>
+        {todos.length === 0 && <p>Não há tarefas</p>}
+      </div>
     </div>
   );
 }
